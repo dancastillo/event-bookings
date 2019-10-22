54d3592a383555586cef96d5d499ce9a739e89af
dancastillo <dancastillo99@gmail.com>
2019-10-22 16:28:21 -0400

    app.js: added headers to allow for cors

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
 
 app.use('/graphql', graphqlHttp({
@@ -27,5 +38,5 @@ const options = {
 };
 
 mongoose.connect(process.env.DB_URI, options)
-  .then(() => app.listen(3000))
+  .then(() => app.listen(8000))
   .catch(err => console.log(err));
